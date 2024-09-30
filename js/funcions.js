a2954522c832c4a402333b049112b97c79d5cfb6
Jimlin829 <jimlin829@gmail.com>
2024-09-30 08:46:35 +0200

    P2 Fet

--- js/funcions.js
+++ js/funcions.js
@@ -1,3 +1,8 @@
 function confirmaRegistre(){
     //completa
+    alert("Registrant estudiant");
+    console.log("Registrant estudiant");
+    var div = document.getElementById("RegDiv");
+    div.innerHTML = "<p class='important'> T'has registrat amb èxit! <p/>";
+    return false;
 }
